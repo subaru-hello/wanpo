8f10388707eb4572ce783806f941823f8ae605c8
subaru-hello <octosubaru0926@gmail.com>
2024-01-27 13:39:23 +0900

    add favorite state which is used everywhere

--- mobile_client/lib/src/models/app_state.dart
+++ mobile_client/lib/src/models/app_state.dart
@@ -2,11 +2,22 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 class AppState extends ChangeNotifier {
+  // 変数を定義
   var current = WordPair.random();
+  var favorites = <WordPair>[];
 
-  // ↓ Add this.
+  // メソッド
   void getNext() {
     current = WordPair.random();
+    notifyListeners();
+  }
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
     notifyListeners();
   }
 }
