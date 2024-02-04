75b357c835935f9cac0162ffa6e92451a4bdf296
subaru-hello <octosubaru0926@gmail.com>
2024-02-05 08:40:14 +0900

    localhostに対応

--- mobile_client/lib/constants/urls.dart
+++ mobile_client/lib/constants/urls.dart
@@ -1,7 +1,21 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 
 final apiUrl = dotenv.env['API_URL'];
-final walkEntryUrl = Uri.https(apiUrl!, "/production/walk-entries");
-final diaryUrl = Uri.https(apiUrl!, "/production/diaries");
-final dogUrl = Uri.https(apiUrl!, "/production/dogs");
+final isLocal = dotenv.env['ENV'] == "development";
+String checkEnv(bool isLocal, String path) {
+  return isLocal ? "/$path" : "/production/$path";
+}
+
+Uri buildUri(String path) {
+  // 環境に応じてパスを調整
+  final adjustedPath = checkEnv(isLocal, path);
+  // ローカル開発環境ではhttp、本番環境ではhttpsを使用
+  return isLocal
+      ? Uri.http(apiUrl!, adjustedPath)
+      : Uri.https(apiUrl!, adjustedPath);
+}
+
+// 関数を呼び出して結果を変数に格納
+final Uri walkEntryUrl = buildUri("walk-entries");
+final Uri diaryUrl = buildUri("diaries");
+final Uri dogUrl = buildUri("dogs");
