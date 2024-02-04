d43c230cfd57440ec4216b3eb92b23738c02b8f8
subaru-hello <octosubaru0926@gmail.com>
2024-02-04 18:54:30 +0900

    feat: rate limit

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,6 +8,8 @@ import { WalkEntryModule } from './walk-entry/walk-entry.module';
 import { AuthModule } from './auth/auth.module';
 import { DogOwnersModule } from './dog-owners/dog-owners.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
   imports: [
     DiaryModule,
@@ -19,8 +21,31 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
