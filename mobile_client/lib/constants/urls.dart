da6571d7854202e9996b330a63d0d10e55455b81
subaru-hello <octosubaru0926@gmail.com>
2024-02-04 22:23:45 +0900

    feat: エンドポイントを修正

--- mobile_client/lib/constants/urls.dart
+++ mobile_client/lib/constants/urls.dart
@@ -2,6 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 final apiUrl = dotenv.env['API_URL'];
-final walkEntryUrl = Uri.https(apiUrl!, "/dev/walk-entries");
-final diaryUrl = Uri.https(apiUrl!, "/dev/diaries");
-final dogUrl = Uri.https(apiUrl!, "/dev/dogs");
+final walkEntryUrl = Uri.https(apiUrl!, "/production/walk-entries");
+final diaryUrl = Uri.https(apiUrl!, "/production/diaries");
+final dogUrl = Uri.https(apiUrl!, "/production/dogs");
--- mobile_client/lib/src/services/fetch_walk_entries.dart
+++ mobile_client/lib/src/services/fetch_walk_entries.dart
@@ -1,5 +1,4 @@
 import 'package:http/http.dart' as http;
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import '../../constants/urls.dart';
 
