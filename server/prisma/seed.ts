70bce55213caab0967cd9bfa69b53eb5d77b64a1
subaru-hello <octosubaru0926@gmail.com>
2024-02-27 07:53:29 +0900

    fix: seedを修正

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -85,6 +85,83 @@ async function main() {
 
     console.log({ wazzy, tama });
   }
+
+  const wazzy = await prisma.dog.findFirst({
+    where: { nickname: 'Wazzy' },
+  });
+  const diaries = await prisma.diary.create({
+    data: {
+      title: 'わじの日記',
+      description: 'わじの毎日を記録する',
+      dogId: wazzy.id,
+    },
+  });
+
+  const walkEntrys = [
+    {
+      title: 'ひやゴンスタジアムの近くを散歩したよ',
+      date: '2024-01-02T00:00:00.000Z',
+      stepCount: 1000,
+      duration: 100,
+      diaryId: diaries.id,
+      summaryImagePath: null,
+    },
+    {
+      title: '平塚の近くを散歩したよ',
+      date: '2024-01-03T00:00:00.000Z',
+      stepCount: 1000,
+      duration: 100,
+      diaryId: diaries.id,
+      summaryImagePath: null,
+    },
+    {
+      title: '代々木公園の近くを散歩したよ',
+      date: '2024-01-06T00:00:00.000Z',
+      stepCount: 2000,
+      duration: 1003,
+      diaryId: diaries.id,
+      summaryImagePath: '0106.png',
+    },
+    {
+      title: '日産スタジアムの近くを散歩したよ',
+      date: '2024-01-04T00:00:00.000Z',
+      stepCount: 50000,
+      duration: 1003,
+      diaryId: diaries.id,
+      summaryImagePath: '0104.png',
+    },
+    {
+      title: '',
+      date: '2024-01-05T00:00:00.000Z',
+      stepCount: 2000,
+      duration: 1003,
+      diaryId: diaries.id,
+      summaryImagePath: '0105.png',
+    },
+    {
+      title: '近くの公園を散歩したよ',
+      date: '2024-01-03T00:00:00.000Z',
+      stepCount: 1000,
+      duration: 100,
+      diaryId: diaries.id,
+      summaryImagePath: null,
+    },
+  ];
+
+  for (const entry of walkEntrys) {
+    const { title, date, stepCount, duration, diaryId, summaryImagePath } =
+      entry;
+    await prisma.walkEntry.createMany({
+      data: {
+        title,
+        date,
+        stepCount,
+        duration,
+        summaryImagePath,
+        diaryId: diaryId,
+      },
+    });
+  }
 }
 
 main()
