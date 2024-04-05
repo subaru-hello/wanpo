bb9cd00cb8842986df3cabca1c02fa0aa6d19d44
subaru-hello <octosubaru0926@gmail.com>
2024-04-06 08:46:48 +0900

    fix: made a top page

--- mobile_client/lib/src/screens/top_page.dart
+++ mobile_client/lib/src/screens/top_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_client/src/screens/dog_page.dart';
 
 class TopPage extends StatefulWidget {
   @override
@@ -8,6 +9,29 @@ class TopPage extends StatefulWidget {
 class _TopPageState extends State<TopPage> {
   @override
   Widget build(BuildContext context) {
-    return Text("topPage");
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Text(
+              "withビション",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DogPage()));
+              },
+              icon: Icon(Icons.pets),
+              label: Text("他のビションを見る"))
+        ],
+      ),
+    );
   }
 }
