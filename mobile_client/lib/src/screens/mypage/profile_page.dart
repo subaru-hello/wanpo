0e378312d03bc7bca9b45f7f4f8717d7724ce96a
subaru-hello <octosubaru0926@gmail.com>
2024-04-17 08:19:21 +0900

    feat: マイページの土台を作成

---
+++ mobile_client/lib/src/screens/mypage/profile_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('マイページ'),
+      ),
+      body: Text("マイページ"),
+    );
+  }
+}
