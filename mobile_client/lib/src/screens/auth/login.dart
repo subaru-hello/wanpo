6eef248a334ae4cdd5c255d6d27a614366358a9b
subaru-hello <octosubaru0926@gmail.com>
2024-04-20 23:12:33 +0900

    fix: layout of login

--- mobile_client/lib/src/screens/auth/login.dart
+++ mobile_client/lib/src/screens/auth/login.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_client/src/api/auth_api.dart';
-import 'package:mobile_client/src/widgets/diary/form.dart';
+import 'package:mobile_client/src/contexts/app_state.dart';
+import 'package:mobile_client/src/route/route.dart';
+import 'package:mobile_client/src/widgets/auth/form.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -27,11 +30,17 @@ class _LoginState extends State<LoginPage> {
   }
 
   Future _submitForm() async {
-    print("hello");
     if (_formKey.currentState!.validate()) {
-      // ここでregisterDiary関数を呼び出す
-      final res = await login(email: email, password: password);
-      print('hello $res');
+      var appState = Provider.of<AppState>(context,
+          listen: false); // Accessing AppState directly
+      final loginRes = await login(email: email, password: password);
+      print(loginRes);
+
+      if (loginRes) {
+        appState.navigateTo(routeHome);
+      } else {
+        // Implement validation message display logic here
+      }
     }
   }
 
@@ -39,20 +48,18 @@ class _LoginState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('ログイン')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            DiaryForm(
+      body: Center(
+        child: Card(
+          margin: EdgeInsets.all(16.0),
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: LoginForm(
               formKey: _formKey,
-              onTitleSaved: _handleEmailSaved,
-              onDeacriptionSaved: _handlePasswordSaved,
+              onEmailSaved: _handleEmailSaved,
+              onPasswordSaved: _handlePasswordSaved,
               onSubmitForm: _submitForm,
             ),
-            // タイトルの表示（デバッグ用）
-            Text('タイトル: $email'),
-            Text('説明書き: $password'),
-          ],
+          ),
         ),
       ),
     );
