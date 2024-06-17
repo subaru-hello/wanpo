d59da25d1bee1a59ebfa3f339ef6f7a57137a172
subaru-hello <octosubaru0926@gmail.com>
2024-06-17 22:35:15 +0900

    feat: signup

---
+++ mobile_client/lib/src/screens/auth/signup.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_client/src/api/auth_api.dart';
+import 'package:mobile_client/src/contexts/app_state.dart';
+import 'package:mobile_client/src/route/route.dart';
+import 'package:mobile_client/src/widgets/auth/sign_up_form.dart';
+import 'package:provider/provider.dart';
+
+// 新規登録→認証コード入力→マイページトップへ遷移
+class SignUpPage extends StatefulWidget {
+  @override
+  State<SignUpPage> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUpPage> {
+  String email = "";
+  String password = "";
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+// すでにログイン済みかどうかを確認するAPIを叩く
+
+  void _handleEmailSaved(String value) {
+    setState(() {
+      email = value;
+    });
+  }
+
+  void _handlePasswordSaved(String value) {
+    setState(() {
+      password = value;
+    });
+  }
+
+  void showPopup(BuildContext context, String errorTxt) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(''),
+          content: Text(errorTxt),
+          actions: <Widget>[
+            TextButton(
+              child: Text('閉じる'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future submitForm() async {
+    if (_formKey.currentState!.validate()) {
+      var appState = Provider.of<AppState>(context,
+          listen: false); // Accessing AppState directly
+      final loginRes = await signUp(email: email, password: password);
+      print(loginRes);
+
+      if (loginRes) {
+        print("succeeded!");
+        appState.navigateTo(routeVerifyCode);
+      } else {
+        if (mounted) {
+          showPopup(context, "新規登録に失敗しました");
+        }
+        // Implement validation message display logic here
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('新規登録')),
+      body: Center(
+        child: Card(
+          margin: EdgeInsets.all(16.0),
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: SignUpForm(
+              formKey: _formKey,
+              onEmailSaved: _handleEmailSaved,
+              onPasswordSaved: _handlePasswordSaved,
+              onSubmitForm: submitForm,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
